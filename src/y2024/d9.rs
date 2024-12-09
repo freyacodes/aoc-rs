887997c4273655b081920782e67681a6159c83e6
Freya Arbjerg <freya@arbjerg.dev>
2024-12-09 19:52:07 +0100

    Clean up day 9 2024 part 1

--- src/y2024/d9.rs
+++ src/y2024/d9.rs
@@ -1,17 +1,13 @@
 use std::time::Instant;
 use crate::util::get_input_string;
 
-fn parse() -> Vec<u8> {
+fn parse() -> Vec<Option<u32>> {
+    let mut disk: Vec<Option<u32>> = vec![];
+    
     get_input_string(2024, 9)
         .chars()
         .map(|c| c.to_digit(10).unwrap() as u8)
-        .collect()
-}
-
-fn part_one() -> u64 {
-    let mut disk: Vec<Option<u32>> = vec![];
-
-    parse().into_iter().enumerate().for_each(|(i, input_digit)| {
+        .enumerate().for_each(|(i, input_digit)| {
         let is_free_space = i % 2 == 1;
 
         if is_free_space {
@@ -21,6 +17,20 @@ fn part_one() -> u64 {
             for _ in 0..input_digit { disk.push(Some(id)); }
         }
     });
+    
+    disk
+}
+
+fn calc_checksum(disk: Vec<Option<u32>>) -> u64 {
+    disk.into_iter()
+        .enumerate()
+        .filter(|(_, f)| f.is_some())
+        .map(|(i,f)| i as u64 * f.unwrap() as u64)
+        .sum()
+}
+
+fn part_one() -> u64 {
+    let mut disk = parse();
 
     let mut low_index = 0;
     let mut high_index = disk.len() - 1;
@@ -42,11 +52,7 @@ fn part_one() -> u64 {
         disk[low_index] = disk[high_index].take();
     }
     
-    disk.into_iter()
-        .filter(|&x| x.is_some())
-        .enumerate()
-        .map(|(i,f)| i as u64 * f.unwrap() as u64)
-        .sum()
+    calc_checksum(disk)
 }
 
 fn part_two() -> u64 {
